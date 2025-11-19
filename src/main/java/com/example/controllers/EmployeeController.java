a3890d23f43d10b6bb9b242b2ca1468bbb32fb77
Sanika1319 <sanikamulikofficial@gmail.com>
2025-11-19 22:34:09 +0530

    Updated features

---
+++ src/main/java/com/example/controllers/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.example.controllers;
+
+import com.example.dto.EmployeeDto;
+import com.example.services.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+   @GetMapping("/{id}")
+    private ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable ("id") Long employeeId){
+        EmployeeDto employeeById = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeById);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> allEmployees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(allEmployees);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable ("id") Long employeeId , @RequestBody EmployeeDto updatedEmployee){
+        EmployeeDto updateEmployee = employeeService.updateEmployee(employeeId, updatedEmployee);
+        return ResponseEntity.ok(updateEmployee);
+    }
+
+}
---
+++ src/main/java/com/example/services/serviceImpl/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.services.serviceImpl;
+
+import com.example.dto.EmployeeDto;
+import com.example.entities.Employee;
+import com.example.exceptions.ResourceNotFoundException;
+import com.example.mapper.EmployeeMapper;
+import com.example.repository.EmployeeRepository;
+import com.example.services.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee is not exist with given id : " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee) ;
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return employeeList.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee)).collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not exists with given id: " + employeeId)
+        );
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updateEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updateEmployee) ;
+    }
+}
